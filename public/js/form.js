55f1e2a71f4661f13dbb8a37efa05167e0d5fea6
Jivin Mazumder <jivinmazumder@debdeeps-air.lan>
2020-07-08 18:10:04 -0400

    Init Commit

---
+++ public/js/form.js
@@ -0,0 +1,11 @@
+// function noDefaults () {
+//     addEventListener('click', function (e) {
+//         e.preventDefault()
+//     })
+// }
+
+const noDefaults = () => {
+    addEventListener('click', e => {
+        e.preventDefault()
+    })
+}
---
+++ public/js/playground/es6.js
@@ -0,0 +1,18 @@
+// var es5 = 'this is es5';
+// var es5 = 'hi';
+
+let es6 = 'this is es6';
+let es6 = 'this is es1';
+
+const myConst = 'this value will never change.';
+const myConst = 'wow';
+
+// function myfunction () {
+//     console.log('hi!')
+// }
+
+const myfunction = everyday => {
+    console.log('hi!')
+}
+
+// Note: comment is es5
